1c6c536e4910759af3d62fcfd76fc0f9bb8a61bb
vishal-ru <vishalrai1office@gmail.com>
2026-01-03 11:09:36 +0530

    Create fact_events table for analytics
    Create fact_events table from stg_events_flat with non-null session IDs.

--- sql /02_session_level_table.sql
+++ sql /02_session_level_table.sql
@@ -1,1 +1,15 @@
+CREATE OR REPLACE TABLE analytics.fact_events AS
+SELECT
+  event_date,
+  event_timestamp,
+  event_name,
+  user_pseudo_id,
+  session_id,
+  device_category,
+  traffic_source,
+  traffic_medium,
+  country,
+  engagement_time_msec
+FROM staging.stg_events_flat
+WHERE session_id IS NOT NULL;
 
