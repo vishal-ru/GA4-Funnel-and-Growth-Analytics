331eff043d6456dc2e92b0e1e6354844a3ea7675
vishal-ru <vishalrai1office@gmail.com>
2026-01-03 11:10:02 +0530

    Create fact_sessions table for funnel metrics

--- sql /03_funnel_metrics.sql
+++ sql /03_funnel_metrics.sql
@@ -1,1 +1,29 @@
+CREATE OR REPLACE TABLE analytics.fact_sessions AS
+SELECT
+  user_pseudo_id,
+  session_id,
+  MIN(event_date)                                    AS session_date,
+  MIN(event_timestamp)                              AS session_start_time,
+
+  COUNT(*)                                          AS total_events,
+  SUM(IFNULL(engagement_time_msec,0))               AS total_engagement_time,
+
+  MAX(IF(event_name = 'view_item', 1, 0))            AS viewed_item,
+  MAX(IF(event_name = 'add_to_cart', 1, 0))          AS added_to_cart,
+  MAX(IF(event_name = 'begin_checkout', 1, 0))       AS began_checkout,
+  MAX(IF(event_name = 'purchase', 1, 0))             AS purchased,
+
+  device_category,
+  traffic_source,
+  traffic_medium,
+  country
+
+FROM analytics.fact_events
+GROUP BY
+  user_pseudo_id,
+  session_id,
+  device_category,
+  traffic_source,
+  traffic_medium,
+  country;
 
